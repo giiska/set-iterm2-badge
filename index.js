34774d6670d0117228b2fed5668e0aa0b2171cc6
John Xiao <xiao-jianfeng@qq.com>
2016-04-14 17:26:20 +0800

    Fix when pass in Number type arg

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Thanks to [@sel-fish](http://stackoverflow.com/questions/36613285/how-to-exec-script-to-set-iterm2-badge-from-nodejs)
 
 function setBadge(rawBadgeFormat) {
-  var base64BadgeFormat = new Buffer(rawBadgeFormat).toString('base64')
+  var base64BadgeFormat = new Buffer(rawBadgeFormat.toString()).toString('base64')
   var setBadgeFormatCmd = 'printf "\\e]1337;SetBadgeFormat=' + base64BadgeFormat + '\\a"'
   require('child_process').exec(setBadgeFormatCmd, function(error, stdout, stderr) {
       if (error) console.log(error);
